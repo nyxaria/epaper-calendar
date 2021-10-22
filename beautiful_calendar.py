488bb357617e0486cd749eda93cd7d9dbc48aa44
George Hartt <georgehartt@Georges-MacBook-Pro.local>
2021-10-23 00:26:18 +0100

    bla

--- beautiful_calendar.py
+++ beautiful_calendar.py
@@ -92,10 +92,10 @@ def draw_short_event(d, e):
     
     """
     x_start = offset_left + bar_left + e["day"] * per_day + e["column"] * per_day / e["max_collision"]
-    y_start = offset_top + bar_top+ offset_allday + math.floor((e["start"] - 60 - (BEGIN_DAY * 60)) * per_hour / 60)
+    y_start = offset_top + bar_top+ offset_allday + math.floor((e["start"] - (BEGIN_DAY * 60)) * per_hour / 60)
     # width = per_day / e["max_collision"]
     width = (epd7in5b_V2.EPD_WIDTH - 2 - offset_left - bar_left) / DAYS
-    y_end = offset_top + bar_top + offset_allday + math.floor((e["end"] - 60 - (BEGIN_DAY * 60)) * per_hour / 60)
+    y_end = offset_top + bar_top + offset_allday + math.floor((e["end"] - (BEGIN_DAY * 60)) * per_hour / 60)
     # clear the event's area and make the outline
     d.rectangle((x_start, y_start, x_start + width, y_end), outline=0, width=2, fill=200)
 
