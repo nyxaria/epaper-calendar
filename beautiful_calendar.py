a4e1712693c2f1d14e26c0da7292088e26ff3f6a
George Hartt <georgehartt@Georges-MacBook-Pro.local>
2021-10-24 23:24:28 +0100

    bla

--- beautiful_calendar.py
+++ beautiful_calendar.py
@@ -114,8 +114,8 @@ def draw_short_event(d, e, other):
     # width = (epd7in5b_V2.EPD_WIDTH - 3 - offset_left - bar_left) / DAYS
     y_end = offset_top + bar_top + offset_allday + math.floor((e["end"] - (BEGIN_DAY * 60)) * per_hour / 60)
     # clear the event's area and make the outline
-    s = e.summary.lower()
-    if "supo" in s or "sally" in s or "assertiveness" in s or "study group" in s or "attention" in s:
+    s = e["title"].lower()
+    if "supo" in s or "sally" in s or "assert" in s or "study" in s or "attention" in s:
         other.rectangle((x_start, y_start, x_start + width, y_end), outline=0, width=2, fill=200)
     else:
         d.rectangle((x_start, y_start, x_start + width, y_end), outline=0, width=2, fill=200)
